1c445dd5dcbca0f1875482778ed2323a5bfbfa43
Piyush <piyush9c24@gmail.com>
2026-05-09 19:53:29 +0530

    token added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,7 +11,7 @@ exports.signup = async (req,res)=>{
         const hashed = await bcrypt.hash(password,10);
         await pool.query("INSERT INTO users(name,password) VALUES($1,$2)",[name,hashed]);//protect from sql injection
         res.status(201).json({message : "User entered"});
-
+        
     }
     catch (err){
         res.status(500).json({error : err.message});
@@ -19,12 +19,15 @@ exports.signup = async (req,res)=>{
 };
 
 exports.login = async(req,res)=>{
-    const { Username , Password}= req.body;
+    const { name , password }= req.body;
     try{
-        const result = await pool.query("SELECT * FROM users WHERE email=$1", [email]);
-        if(result.rows.length==0) return res.status(401);
+        const result = await pool.query("SELECT * FROM users WHERE name=$1", [name]);
+        if (result.rows.length === 0) {
+             return res.status(404).json({ error: "User not found" });
+        }
+       
 
-        const user = result.row[0];
+        const user = result.rows[0];
         const match = await bcrypt.compare(password,user.password);
         if(!match) return res.status(401).json({error : " Invalid password"});
         const token = generateToken({id: user.id , email : user.email});
